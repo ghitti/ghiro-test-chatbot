21bf415a8d9d0c02f5e2e2fd9f9a89294ae40e23
Roberto Ghitti <r.ghitti@gmail.com>
2019-12-27 17:53:12 +0100

    First commit

---
+++ firstBot.py
@@ -0,0 +1,20 @@
+import telepot
+
+def on_chat_message(msg):
+    content_type, chat_type, chat_id = telepot.glance(msg)
+    if content_type == 'text':
+        name = msg["from"]["first_name"]
+        txt = msg['text']
+        bot.sendMessage(chat_id, 'Hello %s, I am a stupid bot!'%name)
+        bot.sendMessage(chat_id, 'I have received this: %s'%txt)
+
+TOKEN = '865509123:AAG7FRgrtssbx2QmSDdYUzOpz9h1Rzz5HpI'
+
+bot = telepot.Bot(TOKEN)
+bot.message_loop(on_chat_message)
+
+print('Listening ...')
+
+import time
+while 1:
+    time.sleep(10)
